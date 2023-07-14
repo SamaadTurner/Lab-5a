51028f898407a996afcf5e1eede983b74d53d39e
SamaadTurner <samaad.turner@gmail.com>
2023-07-14 15:09:24 -0700

    sumArray

--- js/app.js
+++ js/app.js
@@ -69,13 +69,15 @@ function sumAndMultiply(a, b, c) { //eslint-disable-line
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 4
-Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called sumArray() that takes in an array of numbers as its single argument and
+then returns an array where the first element is the sum of the numbers in the array,
+ and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
 "2,3,4 was passed in as an array of numbers, and 9 is their sum."
 
@@ -87,12 +89,13 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+  let testArraySummation = sum(sum(sumArr[0],sumArr[1])[0], sumArr[2])[0];
+  return [testArraySummation, `2,3,4 was passed in as an array of numbers, and ${testArraySummation} is their sum.`];
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -143,3 +146,16 @@ function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 // testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+
+
+
+
+
+
+// let sumCounter;
+//   for(let i = 0; i<sumArr.length; i++){
+//     console.log(sumCounter);
+//     sumCounter = sum(sumArr[i], sumArr[i+1])[0];
+//     console.log(sumCounter);
+//   }
+//   return [ sumCounter,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbersm and is their sum`];
