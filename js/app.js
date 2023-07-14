0aac141306da2ef1b639ca4eca02c8dff308fe1d
SamaadTurner <samaad.turner@gmail.com>
2023-07-14 13:14:28 -0700

    multiply function

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ function sum(a, b) { //eslint-disable-line
 }
 
 // Here is the test for sum(); uncomment it to run it
-testSum(4, 7);
+//testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -31,11 +31,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
+  let arrayQuestionTwo = [];
+  arrayQuestionTwo.push(a * b);
+  arrayQuestionTwo.push('The product of ' + a + ' and ' + b + ' is ' + (a*b) + '.');
+  return arrayQuestionTwo;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
